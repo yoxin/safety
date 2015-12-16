5fa211863045bb1ef7adc18f73215a942904c27b
zhengyoxin <zhengyoxin@163.com>
2015-12-16 15:53:52 +0800

    其他账号上传第二次测试

--- src/com/itheima/mobilesafe/receiver/SMSReceiver.java
+++ src/com/itheima/mobilesafe/receiver/SMSReceiver.java
@@ -15,33 +15,37 @@ public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		//接收短信的代码
+		// 接收短信的代码
 		Object[] objs = (Object[]) intent.getExtras().get("pdus");
-		
+		sp = context.getSharedPreferences("config", context.MODE_PRIVATE);
+
 		for (Object obj : objs) {
-			//具体的某一条短信
+			// 具体的某一条短信
 			SmsMessage sms = SmsMessage.createFromPdu((byte[]) obj);
-			//发送者
+			// 发送者
 			String sender = sms.getOriginatingAddress();
-			//短信内容
+			String safeNumber = sp.getString("safeNumber", "");
+			// 短信内容
 			String body = sms.getMessageBody();
-			if("#*location*#".equals(body)){
-				//获得手机位置地址
-				LogUtil.e(TAG, "获得手机位置地址");
-				//截断广播
-				abortBroadcast();
-			}else if("#*alarm*#".equals(body)){
-				//手机报警
-				LogUtil.e(TAG, "手机报警");
-				abortBroadcast();
-			}else if("#*lockscreen*#".equals(body)){
-				//远程锁屏
-				LogUtil.e(TAG, "远程锁屏");
-				abortBroadcast();
-			}else if("#*wipedata*#".equals(body)){
-				//清除手机数据
-				LogUtil.e(TAG, "清楚数据");
-				abortBroadcast();
+			if (sender.contains(safeNumber)) {
+				if ("#*location*#".equals(body)) {
+					// 获得手机位置地址
+					LogUtil.e(TAG, "获得手机位置地址");
+					// 截断广播
+					abortBroadcast();
+				} else if ("#*alarm*#".equals(body)) {
+					// 手机报警
+					LogUtil.e(TAG, "手机报警");
+					abortBroadcast();
+				} else if ("#*lockscreen*#".equals(body)) {
+					// 远程锁屏
+					LogUtil.e(TAG, "远程锁屏");
+					abortBroadcast();
+				} else if ("#*wipedata*#".equals(body)) {
+					// 清除手机数据
+					LogUtil.e(TAG, "清楚数据");
+					abortBroadcast();
+				}
 			}
 		}
 	}
