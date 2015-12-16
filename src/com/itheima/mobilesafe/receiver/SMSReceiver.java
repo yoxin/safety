7c4e67be7148a68865bb769b6aaa1ebe39fdda8f
zhengyoxin <zhengyoxin@163.com>
2015-12-16 13:34:03 +0800

    其他账号push测试

--- src/com/itheima/mobilesafe/receiver/SMSReceiver.java
+++ src/com/itheima/mobilesafe/receiver/SMSReceiver.java
@@ -5,11 +5,13 @@ import com.itheima.mobilesafe.utils.LogUtil;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.telephony.SmsMessage;
 
 public class SMSReceiver extends BroadcastReceiver {
 
 	private static final String TAG = "SMSReceiver";
+	private SharedPreferences sp;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
