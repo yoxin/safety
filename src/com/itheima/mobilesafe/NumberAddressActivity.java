90e5847778e26029086bfd9d3137657782105f01
zhengyoxin <zhengyoxin@outlook.com>
2015-12-29 08:35:16 +0800

    day4_输入框振动功能

--- src/com/itheima/mobilesafe/NumberAddressActivity.java
+++ src/com/itheima/mobilesafe/NumberAddressActivity.java
@@ -2,6 +2,7 @@ package com.itheima.mobilesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -21,6 +22,10 @@ public class NumberAddressActivity extends Activity {
 	private EditText edt_phone;
 	private TextView txt_address;
 	private Button btn_query;
+	/**
+	 * 手机震动服务
+	 */
+	private Vibrator vibrator;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class NumberAddressActivity extends Activity {
 		edt_phone = (EditText)findViewById(R.id.edt_phone);
 		txt_address = (TextView)findViewById(R.id.txt_address);
 		btn_query = (Button)findViewById(R.id.btn_qurey);
+		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 		btn_query.setOnClickListener(new QueryOnClickListener());
 		edt_phone.addTextChangedListener(new TextWatcher() {
 			//当文本发生变化的时候调
@@ -67,6 +73,12 @@ public class NumberAddressActivity extends Activity {
 			if (TextUtils.isEmpty(phone)) {
 				Animation shake = AnimationUtils.loadAnimation(NumberAddressActivity.this, R.anim.shake);
 				edt_phone.startAnimation(shake);
+				//震动两秒
+				//vibrator.vibrate(2000);
+				//震动规律
+				//-1不重复，非-1为从pattern的指定下标开始重复
+				long[] pattern = {200,200,1000,1000};
+				vibrator.vibrate(pattern, -1);
 				Toast.makeText(NumberAddressActivity.this, "手机号为空", Toast.LENGTH_LONG).show();
 			} else {
 				String address = NumberAddressQueryUtils.queryNumber(phone);
